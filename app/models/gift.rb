6d0ac78047009ffe740837050ff11904b6691eca
Melvin <melvin@alsu.com.do>
2015-10-20 04:19:23 -0400

    Changes for Responsive, Fix age range, clean code

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,7 +1,4 @@
 class Gift < ActiveRecord::Base
   has_many :winners, dependent: :nullify
 
-  scope :in_stock, -> { where('inventory != 0 and type_g != 10') }
-  scope :without_blank, -> {where('type_g != 10') }
-  scope :tin, -> { where(type_g: 5) }
 end
